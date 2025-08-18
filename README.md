1a25ff67c8822a3986cb76f00cd1745a3a54321d
Atos Cunha <atos.silvacunha@gmail.com>
2025-08-18 10:56:08 -0300

    1 go

---
+++ README.md
@@ -0,0 +1,51 @@
+APÓS DOWNLOAD DO PROJETO
+
+
+INSERIR O COMANDO 
+npm init 
+NA RAIZ DO PROJETO
+
+ENTAO SERA REALIZADO O PROCESSO DE 
+UTILIZACAO DO COMANDO 
+npm install
+NOS SEGUINTES DIRETORIOS A PARTIR DO DIRETORIO RAIZ
+
+->
+        cd ./books_react
+
+        npm install
+
+        cd ..
+
+        cd ./books_server
+
+        npm install
+
+
+" ## SE NECESSARIO VERIFICAR OS ARQUIVOS DE node_modules # "
+
+
+
+
+
+
+
+
+
+
+"# books_react" 
+
+"books_server -> app.js"
+
+Abrir o diretorio books_server e pelo terminal iniciar o arquivo index.js 
+com o comando
+
+        -> node app.js
+
+    "books_react -> src/index.js"
+
+Abrir diretorio do books_react 
+acessar diretorio src 
+iniciar arquivo index.js
+
+        -> npm start
---
+++ books_react/src/componentes/UltimosLancamentos/index.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { livros } from './dadosUltimosLancamentos';
+import { Titulo } from '../Titulo';
+import CardRecomenda from '../CardRecomenda';
+import imagemLivro from '../../imagens/livro2.png';
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
+function UltimosLancamentos() {
+    return (
+        <UltimosLancamentosContainer>
+            <Titulo or={"#EB9B00"} tamanhoFonte={"36px"}>ÚLTIMOS LANÇAMENTOS</Titulo>
+            <NovosLivrosContainer>
+                {livros.map(livro => (
+                    <img src={livro.src} alt='livro' />
+                ))}
+            </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
+        </UltimosLancamentosContainer>
+    )
+}
+
+export default UltimosLancamentos;
